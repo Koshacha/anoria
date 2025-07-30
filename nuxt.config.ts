71965acd17dd4c8c8b9892e5a7fdef49d034d025
Ilya M. <qbub@proton.me>
2025-07-31 01:49:47 +0700

    chore: remove boilerplate code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxtjs/tailwindcss",
+    "motion-v/nuxt",
+    // 'nuxt-mongoose'
+  ],
+
+  fonts: {
+    families: [
+      {
+        name: "Inter",
+        provider: "google",
+        weights: ["400", "500", "600", "700"],
+      },
+    ],
+  },
+
+  // mongoose: {
+  //   uri: process.env.MONGO_URI
+  // }
+});
