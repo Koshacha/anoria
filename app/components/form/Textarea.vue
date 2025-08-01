dd4a1311f37760869b7e4f569efee8d5cb48f24f
Ilya M. <qbub@proton.me>
2025-08-02 01:41:26 +0700

    fix: minor bugs

--- app/components/form/Textarea.vue
+++ app/components/form/Textarea.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const model = defineModel<string>();
+</script>
+
 <template>
   <textarea
+    v-model="model"
     rows="6"
     class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary/50 focus:border-transparent outline-none transition-all resize-none"
   ></textarea>
